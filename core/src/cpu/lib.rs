bb5c883c03136c20516d0b237d3686861d171580
Muaaz Bhyat <muu794@gmail.com>
2026-08-27 22:41:53 +0200

    Moved CPU related traits to their own file

--- core/src/cpu/lib.rs
+++ core/src/cpu/lib.rs
@@ -51,53 +51,6 @@ pub enum TargetQuirk {
 pub(crate) struct Quirks {
     quirk_map: u16,
 }
-impl From<u16> for Quirks {
-    fn from(quirk_map: u16) -> Self {
-        Self { quirk_map }
-    }
-}
-impl BitOr for Quirks {
-    type Output = Self;
-    fn bitor(self, other: Self) -> Self::Output {
-        Self {
-            quirk_map: self.quirk_map | other.quirk_map,
-        }
-    }
-}
-impl BitOr<TargetQuirk> for Quirks {
-    type Output = Self;
-    fn bitor(self, other: TargetQuirk) -> Self::Output {
-        Self {
-            quirk_map: self.quirk_map | other as u16,
-        }
-    }
-}
-impl BitOrAssign<TargetQuirk> for Quirks {
-    fn bitor_assign(&mut self, other: TargetQuirk) {
-        self.quirk_map |= other as u16;
-    }
-}
-impl BitAnd for Quirks {
-    type Output = Self;
-    fn bitand(self, other: Self) -> Self::Output {
-        Self {
-            quirk_map: self.quirk_map & other.quirk_map,
-        }
-    }
-}
-impl BitAnd<TargetQuirk> for Quirks {
-    type Output = Self;
-    fn bitand(self, other: TargetQuirk) -> Self::Output {
-        Self {
-            quirk_map: self.quirk_map & other as u16,
-        }
-    }
-}
-impl BitAndAssign<TargetQuirk> for Quirks {
-    fn bitand_assign(&mut self, other: TargetQuirk) {
-        self.quirk_map &= other as u16
-    }
-}
 
 pub(super) const RAM_SIZE: usize = 4096;
 pub(super) const STACK_SIZE: usize = 16;
--- core/src/cpu/traits.rs
+++ core/src/cpu/traits.rs
@@ -1,8 +1,58 @@
-use crate::cpu::Cpu;
+use crate::cpu::{Cpu, TargetQuirk};
 use std::fmt::Display;
+use std::ops::{BitAnd, BitAndAssign, BitOr, BitOrAssign};
+use crate::cpu::lib::Quirks;
 
 impl Display for Cpu {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.display)
+        write!(f, "{}", self.get_display())
+    }
+}
+
+impl From<u16> for Quirks {
+    fn from(quirk_map: u16) -> Self {
+        Self { quirk_map }
+    }
+}
+impl BitOr for Quirks {
+    type Output = Self;
+    fn bitor(self, other: Self) -> Self::Output {
+        Self {
+            quirk_map: self.quirk_map | other.quirk_map,
+        }
+    }
+}
+impl BitOr<TargetQuirk> for Quirks {
+    type Output = Self;
+    fn bitor(self, other: TargetQuirk) -> Self::Output {
+        Self {
+            quirk_map: self.quirk_map | other as u16,
+        }
+    }
+}
+impl BitOrAssign<TargetQuirk> for Quirks {
+    fn bitor_assign(&mut self, other: TargetQuirk) {
+        self.quirk_map |= other as u16;
+    }
+}
+impl BitAnd for Quirks {
+    type Output = Self;
+    fn bitand(self, other: Self) -> Self::Output {
+        Self {
+            quirk_map: self.quirk_map & other.quirk_map,
+        }
+    }
+}
+impl BitAnd<TargetQuirk> for Quirks {
+    type Output = Self;
+    fn bitand(self, other: TargetQuirk) -> Self::Output {
+        Self {
+            quirk_map: self.quirk_map & other as u16,
+        }
+    }
+}
+impl BitAndAssign<TargetQuirk> for Quirks {
+    fn bitand_assign(&mut self, other: TargetQuirk) {
+        self.quirk_map &= other as u16
     }
 }
